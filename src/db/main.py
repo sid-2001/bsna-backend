34d2b5b7fc44e4dffd581cceb353b5dc1a4d1b3b
siddhant kaushik <siddhant.kaushik@impronics.co.za>
2025-04-09 20:18:54 +0530

    fixes

--- src/db/main.py
+++ src/db/main.py
@@ -1,33 +1,30 @@
 import logging
-from sqlmodel import create_engine, text, SQLModel
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
-from src.config import Config
 
 # Suppress SQLAlchemy logs
 logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 logging.getLogger("sqlalchemy.pool").setLevel(logging.WARNING)
 
-# Initialize the database engine
-engine = AsyncEngine(
-    create_engine(
-        url=Config.DB_URL,
-        echo=False  # Disable echo logging
-    )
-)
+# Async database URL (with asyncpg)
+DATABASE_URL = 'postgresql+asyncpg://doadmin:AVNS_xzyC5HsXTawenfBh8Y4@db-postgresql-blr1-15255-do-user-19732516-0.m.db.ondigitalocean.com:25060/bsna_dev'
+
+# Create async engine
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=False)
 
 # Initialize the database
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
 
-# Define the database session object
+# Get DB session
 async def get_session() -> AsyncSession:  # type: ignore
-    Session = sessionmaker(
-        bind=engine,
+    async_session = sessionmaker(
+        engine,
         class_=AsyncSession,
         expire_on_commit=False
     )
-    async with Session() as session:
+    async with async_session() as session:
         yield session
