130b39a367860ddf3653f92a6a047ecedde12239
siddhant kaushik <siddhant.kaushik@impronics.co.za>
2025-05-03 23:37:38 +0530

    fix in port

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from src.drivers.routes import driver_router
 from src.alert_logs.routes import alert_router
 from src.support_schedules.routes import schedule_router
 import uvicorn
+from src.config import Config
 
 
 @asynccontextmanager
@@ -51,4 +52,4 @@ app.include_router(alert_router, prefix="/api/v1/alerts", tags=['alerts'])
 app.include_router(schedule_router, prefix="/api/v1/schedules", tags=['schedules'])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=Config.PORT) 
