5857694cf6ebfc4c685eab6ce77e1210433e2c6e
siddhant.kaushik <siddhant.kaushik@impronics.co.za>
2026-03-11 18:51:43 +0530

    chagnes in model

--- src/alert_logs/models.py
+++ src/alert_logs/models.py
@@ -1,12 +1,14 @@
 from sqlmodel import SQLModel, Field, Column, Relationship
-from datetime import datetime
+from datetime import datetime, timezone
 import uuid
 from sqlalchemy.dialects import postgresql as pg
 from typing import Optional
 from src.users import models
 
+
 class AlertLogs(SQLModel, table=True):
     __tablename__ = "logs"
+
     uid: uuid.UUID = Field(
         sa_column=Column(
             pg.UUID,
@@ -16,6 +18,7 @@ class AlertLogs(SQLModel, table=True):
             primary_key=True
         )
     )
+
     driver_name: str = Field(
         sa_column=Column(
             pg.TEXT,
@@ -25,12 +28,14 @@ class AlertLogs(SQLModel, table=True):
         min_length=4,
         max_length=12
     )
+
     transaction_count: int = Field(
         sa_column=Column(
             pg.INTEGER,
             nullable=False
         )
     )
+
     reason_of_abend: str = Field(
         sa_column=Column(
             pg.TEXT,
@@ -40,29 +45,37 @@ class AlertLogs(SQLModel, table=True):
         min_length=1,
         max_length=250
     )
+
     raised_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(
             pg.TIMESTAMP(timezone=True),
-            nullable=False,
-            default=datetime.now()
+            nullable=False
         )
     )
-    fixed_at: datetime = Field(
+
+    fixed_at: Optional[datetime] = Field(
+        default=None,
         sa_column=Column(
             pg.TIMESTAMP(timezone=True),
             nullable=True
         )
     )
+
     status: str = Field(
         sa_column=Column(
-            pg.ENUM("open","closed","attending", name="log_status"),
+            pg.ENUM("open", "closed", "attending", name="log_status"),
             nullable=False,
             default="open"
         )
     )
+
     attending_person: Optional[uuid.UUID] = Field(
         default=None,
         foreign_key="users.id"
     )
-    
-    attendee : Optional["models.User"] = Relationship(back_populates="alerts",sa_relationship_kwargs={'lazy':'selectin'})
+
+    attendee: Optional["models.User"] = Relationship(
+        back_populates="alerts",
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
